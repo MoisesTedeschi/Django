7d6f338a5def36a6a535f82077fe041ac9170f5a
Moisés Tedeschi <moisestedeschi@gmail.com>
2020-07-05 00:23:04 -0300

    Adicionando o Django Debug Toolbar ao projeto.

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -2,4 +2,11 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Olá, Django!')
+    return HttpResponse(''
+                        '<html>'
+                        '<title>Projeto Django</title>'
+                        '<body>'
+                        '<h1>Olá, Django!</h1>'
+                        '</body>'
+                        '</html>',
+                        content_type='text/html')
--- pypro/settings.py
+++ pypro/settings.py
@@ -68,6 +68,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pypro.wsgi.application'
 
+INTERNAL_IPS=config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+# Configuração Django Debug Tolbar
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases

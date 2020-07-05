0c9eff0befc50e40ba96ba97388ddcb83b0fc93f
Moisés Tedeschi <moisestedeschi@gmail.com>
2020-07-04 23:02:14 -0300

    Migrações feitas, configuração de usuários e usuário admin.

--- pypro/base/admin.py
+++ pypro/base/admin.py
@@ -19,9 +19,9 @@ from django.views.decorators.debug import sensitive_post_parameters
 
 from pypro.base.models import User
 
-
 csrf_protect_m = method_decorator(csrf_protect)
 sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
+
 
 
 @admin.register(User)
